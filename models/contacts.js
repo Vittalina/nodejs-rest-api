5c0817570c3f7bc740579ad9fa42d72b756fcdad
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-10-30 17:05:01 +0200

    commit

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,48 @@
-// const fs = require('fs/promises')
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const contactsPath = path.resolve("./models/contacts.json");
 
-const getContactById = async (contactId) => {}
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  console.table(result);
+};
 
-const removeContact = async (contactId) => {}
+const getContactById = async (contactId) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  const getContact = result.find((contact) => contact.id === contactId);
+  console.table(getContact);
+};
 
-const addContact = async (body) => {}
+const removeContact = async (contactId) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  const newData = result.filter((contact) => contact.id !== contactId);
+  console.table(newData);
+  fs.writeFile(contactsPath, JSON.stringify(newData));
+};
 
-const updateContact = async (contactId, body) => {}
+const addContact = async (name, email, phone) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  result.push({ name, email, phone });
+  console.table(result);
+  fs.writeFile(contactsPath, JSON.stringify(result));
+};
+
+const updateContact = async (contactId, body) => {
+  const data = await fs.readFile(contactsPath, "utf8");
+  const result = JSON.parse(data);
+  const getIndex = result.findIndex((contact) => contact.id === contactId);
+  const contactIndex = result[getIndex];
+  if (contactIndex === -1) {
+    return;
+  }
+  Object.assign(result[getIndex], body);
+  fs.writeFile(contactsPath, JSON.stringify(result));
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +50,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
