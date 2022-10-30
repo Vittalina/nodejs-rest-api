e3cab892b4099b403a0fc25de5f63c5c32a48e82
Vittalina <vitalina.alleksandrovna@gmail.com>
2022-10-30 20:09:42 +0200

    commit

--- models/contacts.js
+++ models/contacts.js
@@ -7,6 +7,7 @@ const listContacts = async () => {
   const data = await fs.readFile(contactsPath, "utf8");
   const result = JSON.parse(data);
   console.table(result);
+  return result;
 };
 
 const getContactById = async (contactId) => {
@@ -14,6 +15,7 @@ const getContactById = async (contactId) => {
   const result = JSON.parse(data);
   const getContact = result.find((contact) => contact.id === contactId);
   console.table(getContact);
+  return getContact;
 };
 
 const removeContact = async (contactId) => {
@@ -21,15 +23,21 @@ const removeContact = async (contactId) => {
   const result = JSON.parse(data);
   const newData = result.filter((contact) => contact.id !== contactId);
   console.table(newData);
+  if (newData.length === result.length) {
+    return false;
+  }
   fs.writeFile(contactsPath, JSON.stringify(newData));
+  return true;
 };
 
 const addContact = async (name, email, phone) => {
   const data = await fs.readFile(contactsPath, "utf8");
   const result = JSON.parse(data);
-  result.push({ name, email, phone });
+  const contact = { name, email, phone };
+  result.push(contact);
   console.table(result);
   fs.writeFile(contactsPath, JSON.stringify(result));
+  return contact;
 };
 
 const updateContact = async (contactId, body) => {
@@ -42,6 +50,7 @@ const updateContact = async (contactId, body) => {
   }
   Object.assign(result[getIndex], body);
   fs.writeFile(contactsPath, JSON.stringify(result));
+  return contactIndex;
 };
 
 module.exports = {
